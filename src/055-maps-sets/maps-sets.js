4ec640c8eadc0d4a86b4b1f802abbdf124e78395
Raju Gandhi <raju.gandhi@gmail.com>
2020-02-24 14:30:29 -0500

    feat: introduce the exercises

---
+++ src/055-maps-sets/maps-sets.js
@@ -0,0 +1,51 @@
+// TODO: Implement this
+// Return a map with 3 entries
+// 614 -> 'Ohio'
+// 303 -> 'Colorado'
+// 209 -> 'California'
+function areaCodeToCity() {
+}
+
+// TODO: Implement this
+// You will be passed 'n' number of two-item
+// tuples, like so [1, 'ek']
+// Return a map with those as the individual entries
+function mapFromIterable() {
+}
+
+// TODO: Implement this
+// Given a map m, and a key and value, this should RETURN A NEW
+// MAP with ALL the keys (including the new one)
+// ONLY if the map does NOT have supplied key.
+// Else just return a new Map constructed from m
+function addToMap(m, key, value) {
+}
+
+// TODO: Implement this
+// Return a new Set with 3 entries
+// with the first three letters of the alphabet
+function alphabet() {
+}
+
+// TODO: Implement this
+// You will be passed 'n' items.
+// Return a set with those as the individual entries
+function setFromIterable(args) {
+}
+
+// TODO: Implement this
+// Given a set s, and a key, this should RETURN A NEW
+// MAP with ALL the keys (including the new one)
+// ONLY if the map does not have supplied key.
+// Else just return a new Map constructed from m
+function addToSet(s, key) {
+}
+
+export {
+  areaCodeToCity,
+  mapFromIterable,
+  addToMap,
+  alphabet,
+  setFromIterable,
+  addToSet,
+};
